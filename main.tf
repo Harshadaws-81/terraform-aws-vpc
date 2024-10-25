5220a926916a12614ab9cfc8dd8c945ad0aec462
harshavardhan <kadiamharsha27@gmail.com>
2024-10-25 12:32:13 +0530

    Terraform - terraform-aws-vpc

--- main.tf
+++ main.tf
@@ -66,3 +66,37 @@ resource "aws_subnet" "database" {
     }
   )
 }
+# DB Subnet group for RDS
+resource "aws_db_subnet_group" "default" {
+  name       = local.resource_name
+  subnet_ids = aws_subnet.database[*].id
+
+  tags = merge(
+    var.common_tags,
+    var.db_subnet_group_tags,
+    {
+      Name = local.resource_name
+    }
+  )
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = merge(
+    var.common_tags,
+    var.nat_gateway_tags,
+    {
+      Name = local.resource_name
+    }
+  )
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.main]
+}
